224a70656b4ee1fac01d4b875121d2d07cea0332
Sagar Patel <sagar.patel19nov@gmail.com>
2023-09-06 19:27:41 -0400

    Implimented changes to UI and added dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Button, ButtonGroup, GridItem } from "@chakra-ui/react";
 import { Grid, Show } from "@chakra-ui/react";
 import "./App.css";
+import NavBar from "./Components/NavBar";
 
 function App() {
   // return <Button colorScheme="blue">Button</Button>;
@@ -11,8 +12,8 @@ function App() {
         lg: `"nav nav" "aside main"`,
       }}
     >
-      <GridItem area="nav" bg="coral">
-        Nav
+      <GridItem area="nav">
+        <NavBar></NavBar>
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" bg="gold">
