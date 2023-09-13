cdc61f2e7f452a54087bff0e2a99bea2d238ed03
Sagar Patel <sagar.patel19nov@gmail.com>
2023-09-12 21:06:30 -0400

    GenreList Skeleton Render

--- src/Components/GenreList.tsx
+++ src/Components/GenreList.tsx
@@ -1,9 +1,14 @@
 import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenre from "../hooks/useGenre";
 import getCroppedImageUrl from "../services/image-url";
+import GenreListSkeleton from "./GenreListSkeleton";
 
 const GenreList = () => {
-  const { data } = useGenre();
+  const { data, isLoading, error } = useGenre();
+
+  if (error) return null;
+  if (isLoading) return <GenreListSkeleton />;
+
   return (
     <List>
       {data.map((genre) => (
---
+++ src/Components/GenreListSkeleton.tsx
@@ -0,0 +1,11 @@
+import { List, SkeletonText } from "@chakra-ui/react";
+
+const GenreListSkeleton = () => {
+  return (
+    <List>
+      <SkeletonText />
+    </List>
+  );
+};
+
+export default GenreListSkeleton;
